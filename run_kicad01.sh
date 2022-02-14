50499b4f89685c11a2e4f7000d1cc82f0d5351f5
Your Name <you@example.com>
2022-02-14 17:00:53 -0500

    x

---
+++ run_kicad01.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd
+
+echo kicad /home/dyn/kicad/kicad01_power_board_for_nuclear_team/kicad01_power_board_for_nuclear_team.kicad_pro $*
+     kicad /home/dyn/kicad/kicad01_power_board_for_nuclear_team/kicad01_power_board_for_nuclear_team.kicad_pro $*
